5e5d1610cdd615834c919e9b1700c2da4a458cad
Jacky Truong <hello.jackytruong@gmail.com>
2021-08-26 21:38:29 +0700

    Route controller

--- src/index.js
+++ src/index.js
@@ -5,9 +5,16 @@ const handlebars = require('express-handlebars');
 const app = express();
 const port = 3000;
 
+const route = require('./routes');
+
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(morgan('combined'));
+app.use(express.urlencoded({
+  extended: true
+}));
+app.use(express.json());
+
+// app.use(morgan('combined'));
 
 // Template engine
 // Template engine
@@ -23,12 +30,9 @@ app.engine(
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'resources', 'views'));
 
-app.get('/', function (req, res) {
-  res.render('home');
-});
-app.get('/news', function (req, res) {
-  res.render('news');
-});
+//ROUTE init
+route(app);
+
 
 
 app.listen(port, () => {
